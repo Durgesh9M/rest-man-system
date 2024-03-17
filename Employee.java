c562f358184897119c1f3a63c7dfc184cdb06881
Durgesh9M <141748094+Durgesh9M@users.noreply.github.com>
2024-03-17 14:21:43 +0530

    added model employee

---
+++ Employee.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import com.mysql.cj.jdbc.Blob;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author durgesh
+ */
+public class Employee {
+    private int eid;
+    private String name;
+    private String category;
+    private String salary;
+    private String timeslot;
+    private String gender;
+    private String mobile;
+    private String email;
+    private String address;
+    private ImageIcon idpicture;
+
+    public ImageIcon getIdpicture() {
+        return idpicture;
+    }
+
+    public void setIdpicture(ImageIcon idpicture) {
+        this.idpicture = idpicture;
+    }
+    
+    public String getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(String timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+}
